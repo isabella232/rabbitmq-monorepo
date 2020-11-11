dd1fc9e6c3ab2bd8d6c808498ae745e28032b33a
Philip Kuryloski <kuryloskip@vmware.com>
2020-11-11 09:47:12 +0100

    WIP: mixed version testing in Actions 4

--- ci/scripts/ct-suite.sh
+++ ci/scripts/ct-suite.sh
@@ -14,8 +14,13 @@ catch() {
     fi
 }
 
+SECONDARY_UMBRELLA_ARGS=""
+if [[ "${SECONDARY_UMBRELLA_VERSION}" != "" ]]; then
+    SECONDARY_UMBRELLA_ARGS="SECONDARY_UMBRELLA=${SECONDARY_UMBRELLA_VERSION}"
+fi
+
 buildevents cmd ${GITHUB_RUN_ID} ${GITHUB_RUN_ID}-${project} ct-${CT_SUITE} -- \
             make ct-${CT_SUITE} \
                  FULL= \
                  FAIL_FAST=1 \
-                 SKIP_AS_ERROR=1
+                 SKIP_AS_ERROR=1 ${SECONDARY_UMBRELLA_ARGS}
